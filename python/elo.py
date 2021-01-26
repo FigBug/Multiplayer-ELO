41c790e227e450424e2fdc377d4c8c8e79428a45
y c <yentl.bilie@hotmail.com>
2021-01-27 00:48:02 +0100

    change: more pythonic code
    python does not need ";"
    python for loop is more like for each loop in other languages

--- python/elo.py
+++ python/elo.py
@@ -10,7 +10,7 @@ class ELOPlayer:
     eloChange = 0
     
 class ELOMatch:
-     def __init__(self):
+    def __init__(self):
         self.players = []
     
     def addPlayer(self, name, place, elo):
@@ -27,43 +27,43 @@ class ELOMatch:
             if p.name == name:
                 return p.eloPost
             
-        return 1500;
+        return 1500
 
     def getELOChange(self, name):
         for p in self.players:
             if p.name == name:
-                return p.eloChange;
+                return p.eloChange
                 
-        return 0;
+        return 0
  
     def calculateELOs(self):
         n = len(self.players)
-        K = 32 / (n - 1);
-        
-        for i in range(n):
-            curPlace = self.players[i].place;
-            curELO   = self.players[i].eloPre;
-                        
-            for j in range(n):
-                if i != j:
-                    opponentPlace = self.players[j].place
-                    opponentELO   = self.players[j].eloPre  
-                    
-                    #work out S
-                    if curPlace < opponentPlace:
-                        S = 1.0
-                    elif curPlace == opponentPlace:
-                        S = 0.5
-                    else:
-                        S = 0.0
-                    
-                    #work out EA
-                    EA = 1 / (1.0 + math.pow(10.0, (opponentELO - curELO) / 400.0))
-                    
-                    #calculate ELO change vs this one opponent, add it to our change bucket
-                    #I currently round at this point, this keeps rounding changes symetrical between EA and EB, but changes K more than it should
-                    self.players[i].eloChange += round(K * (S - EA))
+        K = 32 / (n - 1)
+        for player in self.players:
+            curPlace = player.place
+            curELO   = player.eloPre
 
-                    #add accumulated change to initial ELO for final ELO   
-                    
-            self.players[i].eloPost = self.players[i].eloPre + self.players[i].eloChange
+            for opponent in self.players:
+                if player == opponent:
+                    continue
+                
+                opponentPlace = opponent.place
+                opponentELO   = opponent.eloPre  
+                
+                #work out S
+                if curPlace < opponentPlace:
+                    S = 1.0
+                elif curPlace == opponentPlace:
+                    S = 0.5
+                else:
+                    S = 0.0
+                
+                #work out EA
+                EA = 1 / (1.0 + math.pow(10.0, (opponentELO - curELO) / 400.0))
+                
+                #calculate ELO change vs this one opponent, add it to our change bucket
+                #I currently round at this point, this keeps rounding changes symetrical between EA and EB, but changes K more than it should
+                player.eloChange += round(K * (S - EA))
+
+            #add accumulated change to initial ELO for final ELO   
+            player.eloPost = player.eloPre + player.eloChange
